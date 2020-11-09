c69f0e61f3946f589fecd464e927e8a281a9718c
shln21 <1054790952@qq.com>
2020-11-09 14:44:03 +0800

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+git config user.name "Giert"
+git config user.email "1054790952@qq.com"
+git checkout -b gitbook
+git status
+git add .
+git commit -m "[Travis] Update SUMMARY.md"
+git push -f "https://${GH_TOKEN}@${GH_REF}" gitbook:gitbook
+gitbook install
+gitbook build .
+if [ $? -ne 0 ];then
+    exit 1
+fi
+cd _book
+git init
+git checkout --orphan gh-pages
+git status
+sleep 5
+git add .
+git commit -m "Update gh-pages"
+git remote add origin git@github.com:shln21/blogs.git
+git push -f "https://${GH_TOKEN}@${GH_REF}" gh-pages:gh-pages
